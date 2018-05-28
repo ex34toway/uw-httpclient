0c9d48996dbc5cbb9af3641566c1542919fc2a39
Terry <478409784@qq.com>
2018-05-28 16:42:33 +0800

    返回

--- src/main/java/uw/httpclient/http/HttpMessageCallBack.java
+++ src/main/java/uw/httpclient/http/HttpMessageCallBack.java
@@ -4,16 +4,17 @@ import uw.task.exception.MapperException;
 
 /**
  * Http接口调用成功回调
+ *
  * @author liliang
  * @since 2017/10/10
  */
-@FunctionalInterface
 public interface HttpMessageCallBack {
 
     /**
      * 返回内容转换,比如解析,解密etc
+     *
      * @param content
      * @return
      */
-    String doConvert(String content) throws MapperException ;
+    String doConvert(String content) throws MapperException;
 }
