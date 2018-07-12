c679d57f31592c4d1de52ec381bc0ebab2415f91
liliang <649743135@qq.com>
2018-07-12 10:13:29 +0800

    关于方法的使用场景

--- src/test/java/uw/httpclient/http/get/GetMethodTest.java
+++ src/test/java/uw/httpclient/http/get/GetMethodTest.java
@@ -1,10 +1,12 @@
 package uw.httpclient.http.get;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.common.collect.Maps;
 import org.junit.Test;
 import uw.httpclient.http.HttpInterface;
 import uw.httpclient.http.ResponseWrapper;
 import uw.httpclient.json.JsonInterfaceHelper;
+import uw.httpclient.util.StringUriTemplateUtil;
 
 import java.util.List;
 import java.util.Map;
@@ -41,7 +43,7 @@ public class GetMethodTest {
     }
 
     /**
-     * 关于uriVariables的使用@Terry
+     * 关于uriVariables的使用场景 @Terry
      *
      * @throws Exception
      */
@@ -49,6 +51,23 @@ public class GetMethodTest {
     public void testGetVariablesInUri() throws Exception {
         String resp = httpInterface.getForObject("http://www.baidu.com/s?wd={searchWord}",String.class,
                 "小说");
+        System.out.println(resp);
+    }
+
+    /**
+     * 关于Map<String,String> reqParams 的使用场景 @Terry
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetReqParams() throws Exception {
+        Map<String,Object> reqParams = Maps.newHashMapWithExpectedSize(5);
+        reqParams.put("wd","小说");
+        reqParams.put("ie","utf-8");
+        reqParams.put("ie","utf-8");
+        reqParams.put("rsv_bp",0);
+        reqParams.put("rsv_idx",1);
+        String resp = httpInterface.getForObject(StringUriTemplateUtil.expand("http://www.baidu.com/s", reqParams),String.class);
         System.out.println(resp);
     }
 }
